4c04f1cd2e3b2f8e2a5bcf7aa60641ac5c0316f3
ullah <sheikholeei97@gmail.ocm>
2022-10-22 14:25:19 +0600

    modified something

--- src/Pages/Category/SingleCategory.js
+++ src/Pages/Category/SingleCategory.js
@@ -1,23 +1,25 @@
 import React from 'react'
-import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const SingleCategory = ({_id,image_url, title, total_view, rating, author,details}) => {
 
-    const truncating = (str, num) => {
-        if(str.length > num){
-            return (`${str.slice(0, num)}...`);
-        } else {
-            return str;
-        }
-    };
+    // const truncating = (str, num) => {
+    //     if(str.length > num){
+    //         return (`${str.slice(0, num)}...`);
+    //     } else {
+    //         return str;
+    //     }
+    // };
   return (
     <div className='border border-success my-4 p-2 rounded-2'>
         <img src={image_url} alt="" className='img-fluid' />
         <h3>{title}</h3>
         <h4>Views: {total_view}</h4>
-        <p>{truncating(details, 200)}</p>
-        <Button variant='outline-primary'><Link to={`/news/${_id}`}>Read More</Link></Button>
+        {
+            details.length > 250 ?
+            <p>{details.slice(0, 250) + '...'}<Link to={`/news/${_id}`}>Read More</Link></p> :
+            <p>{details}</p>
+        }
     </div>
   )
 }
--- src/SharedComponents/Header.js
+++ src/SharedComponents/Header.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Container } from 'react-bootstrap';
 import LeftSideBar from './LeftSideBar';
 
@@ -15,19 +14,7 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link><Link to={'/'}>Home</Link></Nav.Link>
           </Nav>
 
           <div className='d-block d-lg-none'>
